485b1173556b2a0ed10fecc1bc2e611fd5a6bc5b
FaustVX <jodu035@gmail.com>
2023-01-03 22:33:25 +0100

    Proper support for ATOM feed

--- Program.cs
+++ Program.cs
@@ -7,12 +7,19 @@ if (args is [string url, string pattern, string xpath] && Uri.TryCreate(url, Uri
     var regex = new Regex(pattern, RegexOptions.Compiled);
     using (var http = new HttpClient())
         xmlDocument.Load(await http.GetStreamAsync(uri));
-    foreach (var node in xmlDocument["rss"]!["channel"]!.OfType<XmlNode>())
-        if (node.Name == "item")
-            if (regex.IsMatch(node["title"]!.FirstChild!.Value!))
-            {
-                var xPathNavigator = node.CreateNavigator()!;
-                Console.WriteLine(xPathNavigator.SelectSingleNode(xpath)!.Value);
-                return;
-            }
+    var first = ((xmlDocument["feed"]?["entry"])??(xmlDocument["rss"]?["channel"]?["item"]));
+    for (var node = (XmlNode)first!; node != null; node = node.NextSibling)
+    {
+        if (!node.HasChildNodes || node["title"] is null)
+            continue;
+        if (regex.IsMatch(node["title"]!.FirstChild!.Value!))
+        {
+            var xPathNavigator = node.CreateNavigator()!.Select(xpath);
+            if (!xPathNavigator.MoveNext())
+                return 1;
+            Console.WriteLine(xPathNavigator.Current!.Value);
+            return 0;
+        }
+    }
 }
+return 1;
