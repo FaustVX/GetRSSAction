cf4d2d9614db5e76b8492fe214ae0087a93a1ce6
FaustVX <jodu035@gmail.com>
2023-01-03 15:40:23 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml;
+using System.Text.RegularExpressions;
+
+if (args is [string url, string pattern, string xpath] && Uri.TryCreate(url, UriKind.Absolute, out var uri))
+{
+    var xmlDocument = new XmlDocument();
+    var regex = new Regex(pattern, RegexOptions.Compiled);
+    using (var http = new HttpClient())
+        xmlDocument.Load(await http.GetStreamAsync(uri));
+    foreach (var node in xmlDocument["rss"]!["channel"]!.OfType<XmlNode>())
+        if (node.Name == "item")
+            if (regex.IsMatch(node["title"]!.FirstChild!.Value!))
+            {
+                var xPathNavigator = node.CreateNavigator()!;
+                Console.WriteLine($"out='{xPathNavigator.SelectSingleNode(xpath)!.Value}'");
+                return;
+            }
+}
